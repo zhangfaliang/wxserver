651bf97a8350aee97c5c91cc5a2a21d181653aba
张发亮 <fl.zhang@8win.com>
2018-07-31 16:17:35 +0800

    add middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require("babel-register")({
   // Optional ignore regex - if any filenames **do** match this regex then they
   // aren't compiled.
   //ignore: /regex/,
-
   // Ignore can also be specified as a function.
   // ignore: function(filename) {
   //   if (filename === "/path/to/es6-file.js") {
@@ -11,48 +10,30 @@ require("babel-register")({
   //     return true;
   //   }
   // },
-
   // Optional only regex - if any filenames **don't** match this regex then they
   // aren't compiled
   //only: /my_es6_folder/,
-
   // Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
   // and .js so you'll have to add them back if you want them to be used again.
   //extensions: [".es6", ".es", ".jsx", ".js"],
-
   // Setting this to false will disable the cache.
   //cache: true
 });
+const XResponseTime = require('../middleware/responseTime').default
+const logger = require('../middleware/logger').default
+console.log(XResponseTime,'55555');
 
-// 应用的其余 require 需要被放到 hook 后面
 const Koa = require('koa');
 const app = new Koa();
 
-// x-response-time
+app.use(XResponseTime);
+app.use(logger);
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  console.log(1);
-  
-  await next();
-  console.log(2);
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-
-// logger
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  console.log(3);
-  await next();
-  console.log(4);
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
+// response
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+
+app.listen(5000);
