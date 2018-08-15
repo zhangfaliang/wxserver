516f2d206b18efe9c212fe0f8b6e1ff802a38685
张发亮 <fl.zhang@8win.com>
2018-08-15 14:29:12 +0800

    add redux

---
+++ src/actions/todo.js
@@ -0,0 +1,28 @@
+//action 是把数据从应用传到store的有效载荷。它是store数据的唯一来源 一般会通过store.disparch()将action传到store
+
+//添加todo 任务的actions是这样的：
+
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  SET_VISIBILITY_FILTER,
+  TOGGLE_TODO
+} from "../constants/todoActionType";
+//Action 创建函数 就是生成 action 的方法。“action” 和 “action 创建函数” 这两个概念很容易混在一起，使用时最好注意区分。
+//在 Redux 中的 action 创建函数只是简单的返回一个 action:
+
+export const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+//这样做将使action创建函数更容易被移至和测试
+
+export const toggleTodo = index => {
+  return { type: TOGGLE_TODO, index };
+};
+
+export const setVisibilityFilter = (filter)=> {
+  return { type :SET_VISIBILITY_FILTER}
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import reactDom from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
 import App from './app';
 import 'reset-css';
 import { Route, BrowserRouter as Router} from 'react-router-dom'
 //import routeConfig from './routes/index';
 import children from './components/example/children';
+import todoApp from './reducers/todo';
 
-
+let store = createStore(todoApp);
+console.log(store.getState(),'99999')
 reactDom.render(
   <App/>,
   document.getElementById("root")
